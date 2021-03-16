600a9fe8e57455be0c5adcab423e4c616e6ed8e6
afashs <dev.afashs@gmail.com>
2021-03-16 21:19:39 +0900

    feature_deployssl_#2: Added envpath

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const port = 4000;
 const fs = require("fs");
 const https = require("https");
+const { KEY_PATH, CERT_PATH } = process.env;
 
 app.use(express.json());
 app.use(cors());
@@ -14,8 +15,8 @@ app.get("/", (req, res) => {
 });
 
 const server = https.createServer({
-  key: fs.readFileSync(`/etc/letsencrypt/live/exitgift.shop/privkey.pem`, 'utf-8'),
-  cert: fs.readFileSync(`/etc/letsencrypt/live/exitgift.shop/cert.pem`, 'utf-8'),
+  key: fs.readFileSync(KEY_PATH, 'utf-8'),
+  cert: fs.readFileSync(CERT_PATH, 'utf-8'),
 }, app).listen(port);
 
 module.exports = server;
