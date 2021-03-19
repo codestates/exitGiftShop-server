25ffc2c77efd7c89367ab4331954d32eeb59de7c
Ubuntu <ubuntu@ip-172-31-43-119.ap-northeast-2.compute.internal>
2021-03-19 15:09:50 +0000

    deploy

--- index.js
+++ index.js
@@ -38,14 +38,14 @@ app.use("/auction", auctionRouter);
 const server = https
   .createServer(
     {
-      // key: fs.readFileSync("/" + process.env.KEY_PATH, "utf-8"),
-      // cert: fs.readFileSync("/" + process.env.CERT_PATH, "utf-8"),
-      key: fs.readFileSync(__dirname + "/key.pem", "utf-8"),
-      cert: fs.readFileSync(__dirname + "/cert.pem", "utf-8"),
+      key: fs.readFileSync("/" + process.env.KEY_PATH, "utf-8"),
+      cert: fs.readFileSync("/" + process.env.CERT_PATH, "utf-8"),
+      // key: fs.readFileSync(__dirname + "/key.pem", "utf-8"),
+      // cert: fs.readFileSync(__dirname + "/cert.pem", "utf-8"),
     },
     app
   )
-  // .listen(port, () => console.log("https://back.exitgift.shop:4000"));
-.listen(port, () => console.log("https://localhost:4000"));
+  .listen(port, () => console.log("https://back.exitgift.shop:4000"));
+// .listen(port, () => console.log("https://localhost:4000"));
 
 module.exports = server;
