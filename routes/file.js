723be2aa56a8b06db77e78cf72a56cf3b954c19d
afashs <dev.afashs@gmail.com>
2021-03-26 10:32:08 +0900

    master clear

--- routes/file.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const fileController = require("../controllers/file");
-
-router.get("/", fileController.list);
-router.get("/:id", fileController.preview);
-
-module.exports = router;
